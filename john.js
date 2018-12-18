fba13a4062aa0e7075822859952da0e3ce087a75
Devin Udy <devin.udy@gmail.com>
2018-12-17 20:49:05 -0700

    added firebase initialization

--- john.js
+++ john.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    var config = {
+        apiKey: "AIzaSyAdIivUmWDtf5rzdTD7Zu676BBGMnldDsY",
+        authDomain: "projectapi-bc143.firebaseapp.com",
+        databaseURL: "https://projectapi-bc143.firebaseio.com",
+        projectId: "projectapi-bc143",
+        storageBucket: "projectapi-bc143.appspot.com",
+        messagingSenderId: "630507404860"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
     // Click function for API call to search items and display to page //  
     $(".search").on("click", function () {
         event.preventDefault();
